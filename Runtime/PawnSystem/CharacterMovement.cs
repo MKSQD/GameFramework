3f20ab7c092b11dddf9a08cdcd311cc7e47502d2
unknown <39922492+OneManMonkeySquad@users.noreply.github.com>
2021-02-07 12:57:26 +0100

    Improved Character interpolation

--- Runtime/PawnSystem/CharacterMovement.cs
+++ Runtime/PawnSystem/CharacterMovement.cs
@@ -65,16 +65,18 @@ namespace GameFramework {
         bool onLadder;
 
         // Interpolation
-        double m_InterpolationBackTime = 0.1;
+        double interpolationBackTime = 0.1;
+        double extrapolationLimit = 0.5;
 
         struct State {
             internal double timestamp;
             internal Vector3 pos;
             internal Quaternion rot;
+            internal bool run;
         }
 
-        State[] m_BufferedState = new State[20];
-        int m_TimestampCount;
+        State[] bufferedState = new State[20];
+        int timestampCount;
 
         public void Teleport(Vector3 targetPosition, Quaternion targetRotation) {
             characterController.enabled = false;
@@ -125,6 +127,7 @@ namespace GameFramework {
             bs.Write(transform.position);
             bs.WriteLossyFloat(yaw, 0, 360, 2);
             bs.WriteLossyFloat(viewPitch, minViewPitch, maxViewPitch, 2);
+            bs.Write(run);
         }
 
         public override void Deserialize(BitStream bs) {
@@ -134,10 +137,11 @@ namespace GameFramework {
             var pos = bs.ReadVector3();
             var yaw = bs.ReadLossyFloat(0, 360, 2);
             viewPitch = bs.ReadLossyFloat(minViewPitch, maxViewPitch, 2);
+            var run = bs.ReadBool();
 
             // Shift the buffer sideways, deleting state 20
-            for (int i = m_BufferedState.Length - 1; i >= 1; i--) {
-                m_BufferedState[i] = m_BufferedState[i - 1];
+            for (int i = bufferedState.Length - 1; i >= 1; i--) {
+                bufferedState[i] = bufferedState[i - 1];
             }
 
             // Record current state in slot 0
@@ -145,9 +149,10 @@ namespace GameFramework {
             state.timestamp = Time.timeAsDouble;
             state.pos = pos;
             state.rot = Quaternion.AngleAxis(yaw, Vector3.up);
-            m_BufferedState[0] = state;
+            state.run = run;
+            bufferedState[0] = state;
 
-            m_TimestampCount = Mathf.Min(m_TimestampCount + 1, m_BufferedState.Length);
+            timestampCount = Mathf.Min(timestampCount + 1, bufferedState.Length);
         }
 
         protected void Update() {
@@ -283,32 +288,22 @@ namespace GameFramework {
             character.view.localRotation = Quaternion.AngleAxis(viewPitchLerp, Vector3.left);
 
             // This is the target playback time of the rigid body
-            double interpolationTime = Time.timeAsDouble - m_InterpolationBackTime;
+            double interpolationTime = Time.timeAsDouble - interpolationBackTime;
 
             // Use interpolation if the target playback time is present in the buffer
-            if (m_BufferedState[0].timestamp > interpolationTime) {
-                // Go through buffer and find correct state to play back
-                for (int i = 0; i < m_TimestampCount; i++) {
-                    if (m_BufferedState[i].timestamp <= interpolationTime || i == m_TimestampCount - 1) {
-                        // The state one slot newer (<100ms) than the best playback state
-                        State rhs = m_BufferedState[Mathf.Max(i - 1, 0)];
-                        // The best playback state (closest to 100 ms old (default time))
-                        State lhs = m_BufferedState[i];
+            if (bufferedState[0].timestamp > interpolationTime) {
+                for (int i = 0; i < timestampCount; ++i) {
+                    if (bufferedState[i].timestamp <= interpolationTime || i == timestampCount - 1) {
+                        State rhs = bufferedState[Mathf.Max(i - 1, 0)];
+                        State lhs = bufferedState[i];
 
                         // Use the time between the two slots to determine if interpolation is necessary
                         double length = rhs.timestamp - lhs.timestamp;
-                        float t = 0.0F;
-                        // As the time difference gets closer to 100 ms t gets closer to 1 in 
-                        // which case rhs is only used
-                        // Example:
-                        // Time is 10.000, so sampleTime is 9.900 
-                        // lhs.time is 9.910 rhs.time is 9.980 length is 0.070
-                        // t is 9.900 - 9.910 / 0.070 = 0.14. So it uses 14% of rhs, 86% of lhs
+                        float t = 0.0f;
                         if (length > 0.0001) {
                             t = (float)((interpolationTime - lhs.timestamp) / length);
                         }
-                        //	Debug.Log(t);
-                        // if t=0 => lhs is used directly
+
                         var position = Vector3.Lerp(lhs.pos, rhs.pos, t);
                         transform.localRotation = Quaternion.Slerp(lhs.rot, rhs.rot, t);
 
@@ -319,8 +314,25 @@ namespace GameFramework {
                         else {
                             Teleport(position, transform.rotation);
                         }
+
+                        Debug.DrawLine(transform.position + Vector3.up * 2, transform.position + Vector3.up * 3, Color.green);
                         return;
                     }
+                }
+            }
+            else {
+                // Use extrapolation
+                State latest = bufferedState[0];
+                State latest2 = bufferedState[Mathf.Min(1, timestampCount - 1)];
+
+                float extrapolationLength = (float)(interpolationTime - latest.timestamp);
+                if (extrapolationLength < extrapolationLimit) {
+                    var speed = latest.run ? settings.moveSpeed : settings.runSpeed;
+                    var velocity = (latest.pos - latest2.pos).normalized * speed;
+
+                    Debug.DrawLine(transform.position + Vector3.up * 2, transform.position + Vector3.up * 3, Color.red);
+                    var position = latest.pos + velocity * extrapolationLength;
+                    characterController.Move(position - transform.position);
                 }
             }
         }
@@ -364,15 +376,15 @@ namespace GameFramework {
         void OnDrawGizmosSelected() {
             Gizmos.color = Color.green;
 
-            State lastS = m_BufferedState[0];
-            for (int i = 1; i < m_TimestampCount; ++i) {
-                Gizmos.DrawLine(lastS.pos, m_BufferedState[i].pos);
-                lastS = m_BufferedState[i];
+            State lastS = bufferedState[0];
+            for (int i = 1; i < timestampCount; ++i) {
+                Gizmos.DrawLine(lastS.pos, bufferedState[i].pos);
+                lastS = bufferedState[i];
             }
 
             Gizmos.color = Color.red;
-            for (int i = 0; i < m_TimestampCount; ++i) {
-                Gizmos.DrawSphere(m_BufferedState[i].pos, 0.05f);
+            for (int i = 0; i < timestampCount; ++i) {
+                Gizmos.DrawSphere(bufferedState[i].pos, 0.05f);
             }
         }
 
